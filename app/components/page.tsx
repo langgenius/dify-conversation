cfced102d7d3be734b98204713d88f65b8363e04
crazywoola <li.zheng@dentsplysirona.com>
2023-06-09 16:51:05 +0800

    feat: add buttons

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,5 +1,7 @@
 import styles from './style.module.css'
 import PowerBy from '@/components/power-by'
+import Button from '@/components/button'
+import AcademicCap from '@heroicons/react/24/solid/AcademicCapIcon'
 const Home = () => {
   return <main className={styles.container}>
     <h1>Components</h1>
@@ -10,6 +12,14 @@ const Home = () => {
 
     <div>
       <h2>Button</h2>
+      <Button text="Blue" type='blue' className='mr-2'/>
+      <Button text="Red" type='red' className='mr-2'/>
+      <Button text="Gray" type='gray' className='mr-2'/>
+      <Button text="White" type='white' className='mr-2'/>
+      <Button text="Blue" type='blue' className='mr-2'>
+        <AcademicCap className="h-4 w-4 text-white mr-2" />
+        Hello Cap
+      </Button>
     </div>
 
     <div>
---
+++ components/button/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC, ReactNode } from 'react'
+import cn from 'classnames'
+import styles from './style.module.css'
+interface ButtonProps {
+  text: string
+  type: 'blue' | 'gray' | 'red' | 'white'
+  className?: string
+  onClick?: () => void
+  children?: ReactNode
+}
+
+const Button: FC<ButtonProps> = ({ 
+    text, 
+    children,
+    className,
+    type= 'blue',
+    onClick
+}: ButtonProps) => {
+  return <button
+    onClick={onClick}
+    className={cn(
+      className && className,
+      styles.button,
+      type === 'blue' && styles.blue,
+      type === 'gray' && styles.gray,
+      type === 'red' && styles.red,
+      type === 'white' && styles.white,
+    )}
+  >
+    {children || text}
+  </button>
+}
+
+export default Button
