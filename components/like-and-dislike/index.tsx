6fb0c042812743bb27cc20f22dca6f1418580db0
crazywoola <427733928@qq.com>
2023-06-09 21:15:32 +0800

    feat: add onAction

--- components/like-and-dislike/index.tsx
+++ components/like-and-dislike/index.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.css'
 import cn from 'classnames'
 interface LikeAndDislikeProps {
   status: 'like' | 'dislike' | 'none'
-  onAction?: (action: 'like' | 'dislike') => void
+  onAction?: (action: 'like' | 'dislike' | 'none') => void
 }
 
 const LikeAndDislike: React.FC<LikeAndDislikeProps> = ({
@@ -14,27 +14,35 @@ const LikeAndDislike: React.FC<LikeAndDislikeProps> = ({
     styles.action_container,
   )}>
     {status === 'none' && <>
-      <span className={cn(
+      <span
+        onClick={() => onAction && onAction('like')}
+        className={cn(
         styles.action,
         'hover:bg-blue-50 hover:outline-1 hover:outline-white',
       )}>
         <span className={styles.like} />
       </span>
-      <span className={cn(
+      <span 
+        onClick={() => onAction && onAction('dislike')}
+      className={cn(
         styles.action,
         'hover:bg-red-50 hover:outline-1 hover:outline-white',
       )}>
         <span className={styles.dislike} />
       </span>
     </>}
-    {status === 'like' && <span className={cn(
+    {status === 'like' && <span 
+      onClick={() => onAction && onAction('none')}
+    className={cn(
       styles.action,
       styles.action_like_active,
       'hover:bg-blue-50 hover:outline-1 hover:outline-white',
     )}>
       <span className={styles.like} />
     </span>}
-    {status === 'dislike' && <span className={cn(
+    {status === 'dislike' && <span 
+      onClick={() => onAction && onAction('none')}
+    className={cn(
       styles.action,
       styles.action_dislike_active,
       'hover:bg-red-50 hover:outline-1 hover:outline-white',
