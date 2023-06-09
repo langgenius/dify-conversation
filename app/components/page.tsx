1c2efe3efd20af38c6229be36ea1ab5e329df1a2
crazywoola <li.zheng@dentsplysirona.com>
2023-06-09 15:18:12 +0800

    init project

---
+++ app/components/page.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.css'
+
+const Home = () => {
+  return <main className="flex min-h-screen p-24">
+    <h1 className={styles.red}>Home</h1>
+  </main>
+}
+
+export default Home
