0b8e0f696d7c69200874afd67887bba7775d8b02
crazywoola <427733928@qq.com>
2023-06-09 21:15:38 +0800

    feat: add onAction

--- components/like-and-dislike/index.tsx
+++ components/like-and-dislike/index.tsx
@@ -17,36 +17,36 @@ const LikeAndDislike: React.FC<LikeAndDislikeProps> = ({
       <span
         onClick={() => onAction && onAction('like')}
         className={cn(
-        styles.action,
-        'hover:bg-blue-50 hover:outline-1 hover:outline-white',
-      )}>
+          styles.action,
+          'hover:bg-blue-50 hover:outline-1 hover:outline-white',
+        )}>
         <span className={styles.like} />
       </span>
-      <span 
+      <span
         onClick={() => onAction && onAction('dislike')}
-      className={cn(
-        styles.action,
-        'hover:bg-red-50 hover:outline-1 hover:outline-white',
-      )}>
+        className={cn(
+          styles.action,
+          'hover:bg-red-50 hover:outline-1 hover:outline-white',
+        )}>
         <span className={styles.dislike} />
       </span>
     </>}
-    {status === 'like' && <span 
+    {status === 'like' && <span
       onClick={() => onAction && onAction('none')}
-    className={cn(
-      styles.action,
-      styles.action_like_active,
-      'hover:bg-blue-50 hover:outline-1 hover:outline-white',
-    )}>
+      className={cn(
+        styles.action,
+        styles.action_like_active,
+        'hover:bg-blue-50 hover:outline-1 hover:outline-white',
+      )}>
       <span className={styles.like} />
     </span>}
-    {status === 'dislike' && <span 
+    {status === 'dislike' && <span
       onClick={() => onAction && onAction('none')}
-    className={cn(
-      styles.action,
-      styles.action_dislike_active,
-      'hover:bg-red-50 hover:outline-1 hover:outline-white',
-    )}>
+      className={cn(
+        styles.action,
+        styles.action_dislike_active,
+        'hover:bg-red-50 hover:outline-1 hover:outline-white',
+      )}>
       <span className={styles.dislike} />
     </span>}
 
