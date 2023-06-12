2137d2c95d6ef99c5b2c920f15dc8324ec9524c1
crazywoola <li.zheng@dentsplysirona.com>
2023-06-12 09:18:04 +0800

    feat: remove loading in first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
   return <main className='flex min-h-screen flex-col items-center justify-between p-24'>Hello</main>
 }
