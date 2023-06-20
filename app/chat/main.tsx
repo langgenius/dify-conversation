a88c40515d76f8ee1447d1d8c893c35a8ee5d429
crazywoola <427733928@qq.com>
2023-06-20 20:51:49 +0800

    feat: add state

--- app/chat/main.tsx
+++ app/chat/main.tsx
@@ -1,12 +1,46 @@
 'use client'
-import { FC } from 'react'
+import { FC, useReducer } from 'react'
 import { AppProps, LocaleProps } from '@/interface'
 import Welcome from '@/components/welcome'
 import Form from '@/components/form'
 import XPowerBy, { XPowerByPrivacy } from '@/components/x-power-by'
 import { t } from '@/i18n'
 
+interface IState {
+  inputs: any[]
+  query: string
+  conversation_id: string | null
+  user: string | null
+}
+
+interface IAction {
+  type: string
+  payload: any
+}
+
+function reducer(state: IState, action: IAction): IState {
+  switch (action.type) {
+    case 'SET_QUERY':
+      return {
+        ...state,
+        query: action.payload
+      }
+    default: {
+      return state
+    }
+  }
+}
+
 const Main: FC<AppProps & LocaleProps> = ({ user_input_form, locale }) => {
+  const initialState: IState = {
+    inputs: [],
+    query: '',
+    conversation_id: null,
+    user: null
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   const items = user_input_form.map((itm: any) => {
     const ik = Object.keys(itm)[0]
     const iv = Object.values(itm)[0] as any
