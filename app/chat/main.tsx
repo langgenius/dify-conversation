c1ac28d680c5a809adf39e3097961c365321eb1b
crazywoola <li.zheng@dentsplysirona.com>
2023-06-20 14:18:24 +0800

    feat: add init inputs form

--- app/chat/main.tsx
+++ app/chat/main.tsx
@@ -1,9 +1,48 @@
 'use client'
 import { FC } from 'react'
 import { AppProps, LocaleProps } from '@/interface'
+import Welcome from '@/components/welcome'
+import Form from '@/components/form'
+import XPowerBy, { XPowerByPrivacy } from '@/components/x-power-by'
 
 const Main: FC<AppProps & LocaleProps> = ({}) => {
-  return <div className='flex'></div>
+  const items = [
+    {
+      type: 'text-input',
+      label: '昵称',
+      variable: 'name',
+      required: false,
+      max_length: 48,
+      default: ''
+    },
+    {
+      type: 'select',
+      label: '类型',
+      variable: 'type',
+      required: false,
+      options: ['单板', '双板'],
+      default: ''
+    }
+  ]
+
+  return (
+    <div className='flex flex-col w-full pt-32 px-5 sm:px-8 md:px-72 '>
+      <section className='mb-6'>
+        <Welcome
+          name='Welcome to Chat'
+          description='Chat is a simple chat application.'
+        />
+      </section>
+      <section className='mb-4'>
+        <Form hint='Init Prompt' items={items} />
+      </section>
+
+      <section className='flex items-center justify-between'>
+        <XPowerByPrivacy />
+        <XPowerBy />
+      </section>
+    </div>
+  )
 }
 
 export default Main
