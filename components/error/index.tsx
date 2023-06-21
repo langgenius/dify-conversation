71c2fcf00bf00a02633681c8aa2bbd0608652dd4
crazywoola <li.zheng@dentsplysirona.com>
2023-06-21 17:24:45 +0800

    feat: add details

--- components/error/index.tsx
+++ components/error/index.tsx
@@ -1,11 +1,14 @@
 'use client'
 import React, { FC } from 'react'
-import styles from './style.module.scss'
 
 interface ErrorPageProps {}
 
 const ErrorPage: FC<ErrorPageProps> = ({}) => {
-  return <div className={styles.errors}>Error</div>
+  return (
+    <div className='flex w-full min-h-screen items-center justify-center text-xs text-red-600'>
+      We are sorry, but something went wrong.
+    </div>
+  )
 }
 
 export default ErrorPage
